00ae60b21ab1d10802fc27a6a0e2eefdaac87264
codesScavenger <avraham770@gmail.com>
2016-09-22 18:21:15 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,13 +1,32 @@
+<?php
+include 'header.php';
+?>
 <div>
-<h2>
-    About <i>Car Spot</i>    
-</h2>
-<h3> Where it all began</h3>
+<h1>
+    Our <i>PHP</i> project    
+</h1>
+<h2>What is this project about?</h2>
 
-<p>Do you know that moment when you wish you could reserve a parking spot
-    specially just for you at a certain day, but you couldn't ? Well, that's how
-    Car Spot started. </p>
-<p>The idea is pretty simple, you have your user and the freedom to reserve a 
-    spot, just for you, if there's left</p>
+<p> In this project we planned a system where a user can reserve a single 
+    parking spot in a parking lot for a whole day. To create this we used a 
+    mixture of tools we learned in this semester like MySQL, PHP and others
+    that we learned before such as HTML5,CSS and Java Script.</p>
 
+<p>The idea is pretty simple, you create a user in the login page and you are 
+free to start reserving once for every day. While if you want to become
+an administrator,a special permission will have to be added to your account
+through the database itself (phpMyAdmin). Once you have that permission, you'll
+have a special interface that will enable you to administrate users and their
+reservations</p>
+<h2>For what &amp when was it done?</h2>
+<p>This project is a part of the Web Applications &amp Dynamic Web Interfaces
+Course (35-602) from the Information Science Dept. in Bar-Ilan University. It
+was done in the 2nd semester of the year 5776.</p>
+
+<h2>Who worked on it?</h2>
+<p> In our team we have Ariella Epstein (328606686), Avraham Kauffmann 
+    (333789436)and Orchan Magaramov.</p>
 </div>
+<?php
+include 'footer.php';
+?>

823b3ea2816f92a7133d987f24b0a45d25e02951
codesScavenger <avraham770@gmail.com>
2016-09-23 13:58:51 +0300

    Update about.php

--- about.php
+++ about.php
@@ -6,26 +6,32 @@ include 'header.php';
     Our <i>PHP</i> project    
 </h1>
 <h2>What is this project about?</h2>
-
-<p> In this project we planned a system where a user can reserve a single 
+<p> 
+    In this project we built a system where a user can reserve a single 
     parking spot in a parking lot for a whole day. To create this we used a 
     mixture of tools we learned in this semester like MySQL, PHP and others
-    that we learned before such as HTML5,CSS and Java Script.</p>
+    that we had learn before such as HTML5,CSS and Java Script.</p>
 
-<p>The idea is pretty simple, you create a user in the login page and you are 
+<p>
+    The idea is pretty simple, you create a user in the login page and you are 
 free to start reserving once for every day. While if you want to become
 an administrator,a special permission will have to be added to your account
 through the database itself (phpMyAdmin). Once you have that permission, you'll
 have a special interface that will enable you to administrate users and their
-reservations</p>
+reservations
+</p>
 <h2>For what &amp when was it done?</h2>
-<p>This project is a part of the Web Applications &amp Dynamic Web Interfaces
+<p>
+    This project is a part of the Web Applications &amp Dynamic Web Interfaces
 Course (35-602) from the Information Science Dept. in Bar-Ilan University. It
-was done in the 2nd semester of the year 5776.</p>
+was done in the 2nd semester of the year 5776
+</p>
 
 <h2>Who worked on it?</h2>
-<p> In our team we have Ariella Epstein (328606686), Avraham Kauffmann 
-    (333789436)and Orchan Magaramov.</p>
+<p>
+    In our team we have Ariella Epstein (328606686), Avraham Kauffmann 
+    (333789436)and Orchan Magaramov (317018224).
+</p>
 </div>
 <?php
 include 'footer.php';

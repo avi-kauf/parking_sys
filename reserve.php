5a4a12f5bbee35f81eee8d45a26b711bc67a2a6e
OrchaniousS <bmagramovs3@gmail.com>
2016-10-30 22:00:32 +0200

    Update reserve.php

--- reserve.php
+++ reserve.php
@@ -6,11 +6,25 @@ session_start();
         <link rel="icon" href="media/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="cssAdmin.css">
         <meta charset="UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <title>Reserve A Spot</title>
     </head>
     <body>
         <?php include 'header.php'; ?>
+        <?php if (isset($_SESSION['user'])) {?>
+        
+        <?php }
+        else { ?>
+        
+        <div id="unlog" style="margin-top:100px;text-align:center;">
+            You Are Transferred To Login Page
+            <br>
+            <span id="dots"></span>
+        </div>
+        
+         <?php } ?> 
         <?php include 'footer.php'; ?>
     </body>
 </html>
-
+<script type="text/javascript" src="dots.js"></script>
